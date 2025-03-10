76940e3009c131045ab52385453d1cecf5fecc4e
ivanglpz <miltonivan117@gmail.com>
2025-03-09 18:52:41 -0600

    update  script with upload serie

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import cloudinary from 'cloudinary';
 import dotenv from 'dotenv';
 import * as fs from 'fs';
 import * as path from 'path';
-import episodes from '../latoons/episodes.json' with { type: 'json' };
-import seasons from '../latoons/seasons.json' with { type: 'json' };
+// import episodes from '../latoons/episodes.json' with { type: 'json' };
+// import seasons from '../latoons/seasons.json' with { type: 'json' };
 import series from '../latoons/series.json' with { type: 'json' };
+console.log(series?.length, 'TOTAL DE SERIES');
 
 dotenv.config();
 cloudinary.v2.config({});
@@ -60,50 +61,97 @@ type Episode = {
 
 const removeText = (title: string) => title?.replace(/\D/g, '');
 
-for (const serie of series) {
-  const seaonsOfSeries = seasons
-    ?.filter((e) => e?.serieId === serie?.id)
-    ?.sort((a, b) => {
-      const one = removeText(a?.title);
-      const two = removeText(b?.title);
+// for (const serie of series) {
+//   const seaonsOfSeries = seasons
+//     ?.filter((e) => e?.serieId === serie?.id)
+//     ?.sort((a, b) => {
+//       const one = removeText(a?.title);
+//       const two = removeText(b?.title);
 
-      return Number(one) - Number(two);
+//       return Number(one) - Number(two);
+//     });
+
+//   const seasonsWithEpisodes = seaonsOfSeries?.reduce(
+//     (acc, curr) => {
+//       const payload = {
+//         season: curr,
+//         //@ts-ignore
+//         episodes:
+//           episodes
+//             //@ts-ignore
+//             ?.filter((i) => i?.seasonId === curr?.id)
+//             //@ts-ignore
+//             ?.sort((a, b) => a?.numberEpisode - b?.numberEpisode) ?? [],
+//       };
+
+//       return [...acc, payload];
+//     },
+//     [] as { season: Season; episodes: Episode[] }[],
+//   );
+
+//   const DATA_JSON = {
+//     serie,
+//     seasons: seasonsWithEpisodes,
+//   };
+
+//   const filePath: string = path.join(
+//     publicDir,
+//     `${DATA_JSON?.serie?.slug}.json`,
+//   );
+
+//   await cloudinary.v2.uploader.upload(filePath, {
+//     use_filename: true,
+//     unique_filename: false,
+//     overwrite: true,
+//     folder: 'app/latoons/public',
+//     resource_type: 'raw',
+//   });
+//   fs.writeFileSync(filePath, JSON.stringify(DATA_JSON, null, 2), 'utf-8');
+// }
+
+function getFileNameFromUrl(url: string): string | null {
+  const regex = /\/([^\/]+\.json)$/;
+  const match = url.match(regex);
+  return match ? match[1] : null;
+}
+
+const handle = async () => {
+  try {
+    const PATHS = await cloudinary.v2.api.resources({
+      prefix: 'app/latoons/public',
+      type: 'upload',
+      max_results: 500,
+      resource_type: 'raw',
     });
 
-  const seasonsWithEpisodes = seaonsOfSeries?.reduce(
-    (acc, curr) => {
-      const payload = {
-        season: curr,
-        //@ts-ignore
-        episodes:
-          episodes
-            //@ts-ignore
-            ?.filter((i) => i?.seasonId === curr?.id)
-            //@ts-ignore
-            ?.sort((a, b) => a?.numberEpisode - b?.numberEpisode) ?? [],
-      };
+    if (PATHS && PATHS.resources && PATHS.resources.length > 0) {
+      console.log('Archivos encontrados:', PATHS.resources);
 
-      return [...acc, payload];
-    },
-    [] as { season: Season; episodes: Episode[] }[],
-  );
+      const JSONSERIES = series?.map((e) => {
+        return {
+          ...e,
+          //@ts-ignore
+          resource: PATHS.resources.find((i) =>
+            getFileNameFromUrl(i?.url)?.includes(e?.slug),
+          )?.url,
+        };
+      });
+      const filePath: string = path.join(publicDir, `series.json`);
 
-  const DATA_JSON = {
-    serie,
-    seasons: seasonsWithEpisodes,
-  };
+      await cloudinary.v2.uploader.upload(filePath, {
+        use_filename: true,
+        unique_filename: false,
+        overwrite: true,
+        folder: 'app/latoons/public',
+        resource_type: 'raw',
+      });
+      fs.writeFileSync(filePath, JSON.stringify(JSONSERIES, null, 2), 'utf-8');
+    } else {
+      console.log('No se encontraron archivos en esa ruta.');
+    }
+  } catch (error) {
+    console.log('Error al obtener los archivos:', error);
+  }
+};
 
-  const filePath: string = path.join(
-    publicDir,
-    `${DATA_JSON?.serie?.slug}.json`,
-  );
-
-  await cloudinary.v2.uploader.upload(filePath, {
-    use_filename: true,
-    unique_filename: false,
-    overwrite: true,
-    folder: 'app/latoons/public',
-    resource_type: 'raw',
-  });
-  fs.writeFileSync(filePath, JSON.stringify(DATA_JSON, null, 2), 'utf-8');
-}
+handle();
